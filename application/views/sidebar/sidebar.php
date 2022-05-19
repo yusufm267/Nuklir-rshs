0905360085b7cf5cc99ac0153452970412395f9f
yusufm267 <yusufm267@gmail.com>
2022-05-19 11:16:24 +0700

    try commit

--- application/views/sidebar/sidebar.php
+++ application/views/sidebar/sidebar.php
@@ -116,13 +116,14 @@
   /** add active class and stay opened when selected **/
   var url = window.location;
 
+  // for sidebar menu entirely but not cover treeview
   $('ul.nav-sidebar a').filter(function(){
-
     return this.href == url;
-
   }).addClass('active');
 
+  // for treeview
   $('ul.nav-treeview a').filter(function(){
     return this.href == url;
   }).parentsUntil(".nav-sidebar > .nav-treeview").addClass('menu-open').prev('a').addClass('active');
+  
 </script>
